6763fb538c0899c1ee378537211ecb0997a48333
Eren-A1998 <erenyatef.g@gmail.com>
2021-06-21 12:44:41 +0200

    profile page ui

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -6,6 +6,7 @@ import { ScrollView, StyleSheet, Image, Text } from "react-native";
 const Profile = (props) => {
   return (
     <ScrollView>
+      <View style={{backgroundColor:"#01366f" , paddingBottom:50,borderBottomEndRadius:50,borderBottomStartRadius:50}}>
       <View style={styles.profileImgContainer}>
         <Image
           style={styles.profileImg}
@@ -15,7 +16,10 @@ const Profile = (props) => {
       <Text style={styles.name}>Jane Doe</Text>
       <Text style={styles.email}>Jane Doe@gmail.com</Text>
 
-      <View  style={{alignSelf:'center',justifyContent:'center'}}>
+      </View>
+
+
+      <View  style={styles.btn_container}>
         <Button style={styles.btn} onPress={() => {
           props.navigation.navigate('Edit')
         }}>
@@ -40,42 +44,51 @@ export default Profile;
 
 const styles = StyleSheet.create({
   profileImgContainer: {
-    // height: 300,
-    // width: 200,
-
-    // display:"flex",
-    //borderRadius: 40,
-    // flexDirection: "column",
-    marginTop: 50,
+    marginTop: 30,
     justifyContent: 'center',
     alignItems: 'center'
   },
   profileImg: {
-    height: 200,
-    width: 200,
+    height: 150,
+    width: 150,
     borderRadius: 100,
     //alignSelf:"center"
   },
   name: {
-    marginTop: 10,
+    color:"white",
+    marginTop: 5,
     fontWeight: "bold",
     fontSize: 30,
     textAlign: "center"
   },
   email: {
-    marginTop: 10,
-    fontSize: 20,
+    color:"white",
+    marginTop: 5,
+    fontSize: 18,
     textAlign: "center"
+  },
+
+  btn_container:{
+    display:'flex',
+    alignSelf:'stretch',
+    justifyContent:'center',
+    padding:20,
+    //backgroundColor:"#01366f",
+    marginTop:50,
+    borderTopLeftRadius:70,
+    borderTopRightRadius:70
   },
 
   btn: {
     marginTop: 30,
     padding: 30,
+    color:'white',
     borderRadius: 20,
-    backgroundColor: '#6aafd5',
+    backgroundColor: '#ff7800',
+    alignSelf:'center',
 
     shadowColor: '#000',
-    shadowOffset: { width: 10, height: 5 },
+    shadowOffset: { width: 100, height: 20 },
     shadowOpacity: 0.8,
     shadowRadius: 2,
     elevation: 6
